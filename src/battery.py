f65c7dba18b33411e1b4ced90067d75c839d1262
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-02-25 23:42:14 +0100

    Rename performance -> efficiency

--- src/battery.py
+++ src/battery.py
@@ -2,9 +2,9 @@ from src.energy_types import Ah
 
 
 class Battery:
-    def __init__(self, capacity: Ah, performance: float, current_charge: Ah = 0):
+    def __init__(self, capacity: Ah = 100, efficiency: float = 1.0, current_charge: Ah = 0):
         self.capacity = capacity
-        self.performance = performance
+        self.efficiency = efficiency
         self.current_charge = current_charge
 
     def charge(self, amount: Ah):
