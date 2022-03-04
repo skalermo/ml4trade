ebabeaadc34abf08a49836698e958040537a2ecd
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-03-04 22:57:23 +0100

    Add basic market buy/sell implementation

--- src/market.py
+++ src/market.py
@@ -1,2 +1,16 @@
 class EnergyMarket:
-    pass
+    def __init__(self, buy_price, sell_price):
+        self.buy_price = buy_price
+        self.sell_price = sell_price
+
+    def buy(self, amount, price_threshold) -> float:
+        # consumer buys `amount` of energy at buy price not higher than `price_threshold`
+        if self.buy_price <= price_threshold:
+            return amount * self.buy_price
+        return 0.0
+
+    def sell(self, amount, price_threshold) -> float:
+        # consumer sells `amount` of energy at sell price not lower than `price_threshold`
+        if self.buy_price >= price_threshold:
+            return amount * self.buy_price
+        return 0.0
