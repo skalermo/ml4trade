0cd918a86bed33f0b166f9992604b5108afc840e
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-03-09 01:15:03 +0100

    Make sure step() works properly timewise

--- test/test_simulation_env.py
+++ test/test_simulation_env.py
@@ -35,8 +35,8 @@ class TestSimulationEnv(unittest.TestCase):
 
     def test_is_now_scheduling_time(self):
         start_datetime = datetime(2022, 1, 1)
-        scheduling_time = start_datetime + timedelta(hours=1)
-        env = SimulationEnv(start_datetime=start_datetime, scheduling_time=scheduling_time.time())
+        scheduling_time = (start_datetime + timedelta(hours=1)).time()
+        env = SimulationEnv(start_datetime=start_datetime, scheduling_time=scheduling_time)
 
         self.assertFalse(env.is_now_scheduling_time())
         time_travel(env, timedelta(hours=1))
@@ -46,14 +46,22 @@ class TestSimulationEnv(unittest.TestCase):
 
     def test_is_now_action_replacement_time(self):
         start_datetime = datetime(2022, 1, 1)
-        action_replacement_time = start_datetime + timedelta(hours=1)
-        env = SimulationEnv(start_datetime=start_datetime, action_replacement_time=action_replacement_time.time())
+        action_replacement_time = (start_datetime + timedelta(hours=1)).time()
+        env = SimulationEnv(start_datetime=start_datetime, action_replacement_time=action_replacement_time)
 
         self.assertFalse(env.is_now_action_replacement_time())
         time_travel(env, timedelta(hours=1))
         self.assertTrue(env.is_now_action_replacement_time())
         time_travel(env, timedelta(hours=1))
         self.assertFalse(env.is_now_action_replacement_time())
+
+    def test_prev_step_returns_on_scheduling_time(self):
+        start_datetime = datetime(2022, 1, 1)
+        scheduling_time = (start_datetime + timedelta(hours=10)).time()
+        env = SimulationEnv(start_datetime=start_datetime, scheduling_time=scheduling_time)
+
+        env.step(None)
+        self.assertTrue(env.is_now_scheduling_time())
 
 
 if __name__ == '__main__':
