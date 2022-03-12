8cb7c9b132304602b6362ddfdcafce36aee4d504
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-03-12 15:55:56 +0100

    Add battery efficiency tests

--- test/test_battery.py
+++ test/test_battery.py
@@ -31,6 +31,21 @@ class TestBattery(unittest.TestCase):
         self.assertEqual(discharged, kWh(40))
         self.assertEqual(battery.current_charge, kWh(0))
 
+    def test_efficiency_cuts_down_energy_charged(self):
+        battery = Battery(kWh(100), 0.25, kWh(50))
+        charged = battery.charge(kWh(100))
+        # not sure whether we want to return amount of
+        # energy used or energy stored in battery
+        self.assertEqual(charged, kWh(100))
+        self.assertEqual(battery.current_charge, kWh(75))
+
+    def test_efficiency_does_not_affect_discharge(self):
+        battery = Battery(kWh(100), 0.25, kWh(50))
+        discharged = battery.discharge(kWh(100))
+
+        self.assertEqual(discharged, kWh(50))
+        self.assertEqual(battery.current_charge, kWh(0))
+
 
 if __name__ == '__main__':
     unittest.main()
