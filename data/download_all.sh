286655242e558445453689f6501a9521c2dcbe3b
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-03-17 23:47:10 +0100

    Add some comments

--- data/download_all.sh
+++ data/download_all.sh
@@ -1,8 +1,10 @@
 mkdir -p ".data"
 
+# download energy prices from google drive
 energy_prices_link="https://drive.google.com/uc\?id\=1wU_zVNQoMtwc7XX1W3WZIbfBa5tuZ5oG\&export\=download"
 wget -N -O ".data/prices_pl.csv" "$energy_prices_link"
 
+# download weather synoptic data from 2016-2022
 weather_link="https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/synop/"
 for year in {2016..2022}
 do
@@ -12,6 +14,7 @@ done
 unzip_output_dir=".data/weather_unzipped_flattened"
 mkdir -p "$unzip_output_dir"
 
+# unzip downloaded weather data
 find . -name "*.zip" |
 while read filename
 do
