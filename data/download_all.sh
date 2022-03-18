2513973ea0e1bead0c53cc1df63549e335738966
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-03-18 01:32:57 +0100

    Fix passing link to wget

--- data/download_all.sh
+++ data/download_all.sh
@@ -1,11 +1,11 @@
 mkdir -p ".data"
 
 # download energy prices from google drive
-energy_prices_link="https://drive.google.com/uc\?id\=1wU_zVNQoMtwc7XX1W3WZIbfBa5tuZ5oG\&export\=download"
+energy_prices_link=https://drive.google.com/uc\?id\=1wU_zVNQoMtwc7XX1W3WZIbfBa5tuZ5oG\&export\=download
 wget -N -O ".data/prices_pl.csv" "$energy_prices_link"
 
 # download weather synoptic data from 2016-2022
-weather_link="https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/synop/"
+weather_link=https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/synop/
 for year in {2016..2022}
 do
     wget -rkN -np -nd -R "index.html*" -P ".data/weather/$year" "$weather_link/$year/"
