b316a0f8977f3a4dd7d72040257d94b2231d47a4
Agnieszka Ewa Dąbrowska <a.e.dabrowska@stud.elka.pw.edu.pl>
2022-03-24 22:44:59 +0100

    Add test for consumption system logic

--- test/energy manipulation/test_consumption.py
+++ test/energy manipulation/test_consumption.py
@@ -1,9 +1,19 @@
+import datetime
 import unittest
+
+from src.custom_types import kW
+from src.energy_manipulation.consumption import ConsumptionSystem
 
 
 class TestConsumption(unittest.TestCase):
     def test_consumption(self):
-        self.assertEqual(True, False)  # add assertion here
+        consumption_system = ConsumptionSystem()
+        power_sum = kW(0)
+        for i in range(100):
+            power = consumption_system.get_power(datetime.date)
+            power_sum += power
+        mean = power_sum.value / 100
+        self.assertAlmostEqual(mean, 5.47, 1)
 
 
 if __name__ == '__main__':
