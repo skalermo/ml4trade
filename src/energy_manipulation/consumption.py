d853856147c272c61926ac8f5fc692ec46405903
Agnieszka Ewa Dąbrowska <a.e.dabrowska@stud.elka.pw.edu.pl>
2022-03-24 13:00:08 +0100

    Chane logic for consumption - average daily energy consumption with gaussian distribution

--- src/energy_manipulation/consumption.py
+++ src/energy_manipulation/consumption.py
@@ -4,7 +4,7 @@ from src.custom_types import kW
 
 
 def _default_callback(date):
-    return kW(5.47*(1+random.gauss(5.47, 0.3)))
+    return kW(random.gauss(5.47, 0.3))
     # 5,47 - average daily energy consumption per power consumer
     # (1996 kWh per power consumer per year according to data from
     # https://www.cire.pl/artykuly/serwis-informacyjny-cire-24/gus-zuzycie-energii-elektrycznej-w-gosp-domowych-wzroslo-o-3-rr-w-2020-r
---
+++ test/energy manipulation/test_consumption.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestConsumption(unittest.TestCase):
+    def test_consumption(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
