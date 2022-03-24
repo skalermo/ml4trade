549e379162968e897e36575cf7e10c56e3c1b662
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-03-24 11:57:26 +0100

    Remove dummy test

--- test/test_battery.py
+++ test/test_battery.py
@@ -50,9 +50,6 @@ class TestBattery(unittest.TestCase):
         with self.assertRaises(AssertionError):
             Battery(kWh(100), 0, kWh(50))
 
-    def test_dummy(self):
-        self.fail()
-
 
 if __name__ == '__main__':
     unittest.main()
