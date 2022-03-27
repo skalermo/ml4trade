35b50fe0a7ddf25fa4c37bb4f59e6e047ded8e23
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-03-27 16:23:12 +0200

    Add tests to get_scheduled...() functions (#9)
    * Rename test file so now it is visible
    
    * Add tests to get_scheduled...() functions
    
    * Add numpy to requirements.txt

---
+++ test/prosumer/test_get_scheduled.py
@@ -0,0 +1,97 @@
+import unittest
+from datetime import time
+
+from src.simulation_env import SimulationEnv
+from src.custom_types import kWh, Currency
+
+
+class GetScheduledBuyAmount(unittest.TestCase):
+    def setUp(self):
+        self.env = SimulationEnv()
+
+    def test_if_none_returns_zero(self):
+        amount = self.env.prosumer.get_scheduled_buy_amount(time(0, 0))
+        self.assertEqual(amount, kWh(0))
+
+    def test_returns_scheduled_value(self):
+        action = self.env.action_space.sample()
+        self.env.step(action)
+        amount = self.env.prosumer.get_scheduled_buy_amount(time(0, 0))
+        self.assertEqual(amount, kWh(action[0]))
+
+    def test_cant_have_negative_value(self):
+        action = self.env.action_space.sample()
+        action[0] = -1
+        self.env.prosumer.scheduled_trading_amounts = action[:48]
+        with self.assertRaises(AssertionError):
+            self.env.prosumer.get_scheduled_buy_amount(time(0, 0))
+
+
+class GetScheduledSellAmount(unittest.TestCase):
+    def setUp(self):
+        self.env = SimulationEnv()
+
+    def test_if_none_returns_zero(self):
+        amount = self.env.prosumer.get_scheduled_sell_amount(time(0, 0))
+        self.assertEqual(amount, kWh(0))
+
+    def test_returns_scheduled_value(self):
+        action = self.env.action_space.sample()
+        self.env.step(action)
+        amount = self.env.prosumer.get_scheduled_sell_amount(time(0, 0))
+        self.assertEqual(amount, kWh(action[24 + 0]))
+
+    def test_cant_have_negative_value(self):
+        action = self.env.action_space.sample()
+        action[24 + 0] = -1
+        self.env.prosumer.scheduled_trading_amounts = action[:48]
+        with self.assertRaises(AssertionError):
+            self.env.prosumer.get_scheduled_sell_amount(time(0, 0))
+
+
+class GetScheduledBuyPriceThreshold(unittest.TestCase):
+    def setUp(self):
+        self.env = SimulationEnv()
+
+    def test_if_none_returns_zero(self):
+        amount = self.env.prosumer.get_scheduled_buy_price_threshold(time(0, 0))
+        self.assertEqual(amount, Currency(0))
+
+    def test_returns_scheduled_value(self):
+        action = self.env.action_space.sample()
+        self.env.step(action)
+        amount = self.env.prosumer.get_scheduled_buy_price_threshold(time(0, 0))
+        self.assertEqual(amount, Currency(action[48 + 0]))
+
+    def test_cant_have_negative_value(self):
+        action = self.env.action_space.sample()
+        action[48 + 0] = -1
+        self.env.prosumer.scheduled_price_thresholds = action[48:]
+        with self.assertRaises(AssertionError):
+            self.env.prosumer.get_scheduled_buy_price_threshold(time(0, 0))
+
+
+class GetScheduledSellPriceThreshold(unittest.TestCase):
+    def setUp(self):
+        self.env = SimulationEnv()
+
+    def test_if_none_returns_zero(self):
+        amount = self.env.prosumer.get_scheduled_sell_price_threshold(time(0, 0))
+        self.assertEqual(amount, Currency(0))
+
+    def test_returns_scheduled_value(self):
+        action = self.env.action_space.sample()
+        self.env.step(action)
+        amount = self.env.prosumer.get_scheduled_sell_price_threshold(time(0, 0))
+        self.assertEqual(amount, Currency(action[48 + 24 + 0]))
+
+    def test_cant_have_negative_value(self):
+        action = self.env.action_space.sample()
+        action[48 + 24 + 0] = -1
+        self.env.prosumer.scheduled_price_thresholds = action[48:]
+        with self.assertRaises(AssertionError):
+            self.env.prosumer.get_scheduled_sell_price_threshold(time(0, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
