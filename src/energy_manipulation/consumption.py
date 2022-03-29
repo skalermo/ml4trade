fe9af9e19912a2e75559794756de8a6f48a14205
Agnieszka Ewa Dąbrowska <a.e.dabrowska@stud.elka.pw.edu.pl>
2022-03-30 00:04:00 +0200

    Change consumption system logic

--- src/energy_manipulation/consumption.py
+++ src/energy_manipulation/consumption.py
@@ -5,13 +5,14 @@ import random
 from src.custom_types import kW
 
 
-def _default_callback(time):
-    return kW(-10)
+def _default_callback(hour, df):
+    consumed_energy = df.iat[hour, 1]
+    return kW(consumed_energy*abs(1+random.gauss(0, 0.3)))
 
 
 class ConsumptionSystem:
 
     def get_power(self, _time: time, callback: Callable = _default_callback) -> kW:
-        df = pandas.read_table('https://github.com/skalermo/ml4trade/blob/consumption_system/consumption_energy.txt')
-
-        return callback(time)
+        # df = pandas.read_table(r'https://github.com/skalermo/ml4trade/blob/consumption_system/consumption_energy.txt')
+        df = pandas.read_table(r'C:\Users\agnie\Desktop\Agnieszka\Nauka\Informatyka\magisterka\mgr\consumption_energy.txt')
+        return callback(_time.hour, df)
