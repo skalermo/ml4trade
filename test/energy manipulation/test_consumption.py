d38b9b95ce7f3de927590b018c28a0f68d15d18a
Agnieszka Ewa Dąbrowska <a.e.dabrowska@stud.elka.pw.edu.pl>
2022-03-30 19:05:45 +0200

    Update test_consumption_per_day

--- test/energy manipulation/test_consumption.py
+++ test/energy manipulation/test_consumption.py
@@ -9,19 +9,23 @@ class TestConsumption(unittest.TestCase):
     def test_consumption(self):
         consumption_system = ConsumptionSystem()
         power_sum = kW(0)
-        for i in range(100):
+        for i in range(10):
             power = consumption_system.get_power(datetime.time(12))
             power_sum += power
-        mean = power_sum.value / 100
+        mean = power_sum.value / 10
         self.assertAlmostEqual(mean, 0.24, 1)
 
     def test_consumption_per_day(self):
         consumption_system = ConsumptionSystem()
         power_sum = kW(0)
-        for i in range(24):
-            power = consumption_system.get_power(datetime.time(i))
-            power_sum += power
-        self.assertAlmostEqual(power_sum.value, 5.38, 1)
+        for j in range(10):
+            daily_sum = kW(0)
+            for i in range(24):
+                power = consumption_system.get_power(datetime.time(i))
+                daily_sum += power
+            power_sum += daily_sum
+        mean = power_sum.value / 10
+        self.assertAlmostEqual(mean, 5.38, 1)
 
 
 if __name__ == '__main__':
