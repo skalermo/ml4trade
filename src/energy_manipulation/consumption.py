e920db578418f7bf59f2474db68905917d46f006
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-04-05 00:41:21 +0200

    Inline energy consumption data inside its processing script

--- src/energy_manipulation/consumption.py
+++ src/energy_manipulation/consumption.py
@@ -1,22 +1,23 @@
 from datetime import time
-from typing import Callable
-import pandas
 import random
-
-from pandas import DataFrame
 
 from src.custom_types import kW
 
-ENERGY_CONSUMPTION_PATH = './consumption_energy.txt'
-
-
-def _default_callback(_time: time, df: DataFrame) -> kW:
-    consumed_energy = df.iat[_time.hour, 1]
-    return kW(consumed_energy * abs(1 + random.gauss(0, 0.03)))
+# avg energy consumption per household
+# for each hour of the day
+energy_consumption_kWh = [
+    0.189, 0.184, 0.181, 0.181, 0.182, 0.185,
+    0.194, 0.222, 0.238, 0.246, 0.246, 0.246,
+    0.248, 0.249, 0.246, 0.244, 0.244, 0.244,
+    0.243, 0.246, 0.246, 0.242, 0.225, 0.208
+]
 
 
 class ConsumptionSystem:
+    def calculate_power(self, _time: time) -> kW:
+        return self._calculate_power(_time)
 
-    def get_power(self, _time: time, callback: Callable = _default_callback) -> kW:
-        df = pandas.read_table(ENERGY_CONSUMPTION_PATH)
-        return callback(_time, df)
+    @staticmethod
+    def _calculate_power(_time: time) -> kW:
+        consumed_energy = energy_consumption_kWh[_time.hour]
+        return kW(consumed_energy * abs(1 + random.gauss(0, 0.03)))
