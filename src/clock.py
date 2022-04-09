99aa11990b57a07edc1f8653b60f050d4cc24ade
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-04-09 19:51:38 +0200

    Add more env logic into the clock class

--- src/clock.py
+++ src/clock.py
@@ -1,10 +1,16 @@
 from datetime import datetime, time, timedelta
 
 
-class Clock:
+from src.constants import SCHEDULING_TIME, ACTION_REPLACEMENT_TIME
+
+
+class SimulationClock:
     def __init__(self, start_datetime: datetime,
+                 scheduling_time: time = SCHEDULING_TIME, action_replacement_time: time = ACTION_REPLACEMENT_TIME,
                  start_tick: int = 0, tick_duration: timedelta = timedelta(hours=1)):
         self.cur_datetime = start_datetime
+        self.scheduling_time = scheduling_time
+        self.action_replacement_time = action_replacement_time
         self.cur_tick = start_tick
         self.tick_duration = tick_duration
 
@@ -12,5 +18,8 @@ class Clock:
         self.cur_tick += 1
         self.cur_datetime += self.tick_duration
 
-    def is_it_time(self, time_to_check: time) -> bool:
-        return self.cur_datetime.time() == time_to_check
+    def is_it_scheduling_hour(self) -> bool:
+        return self.scheduling_time.hour == self.cur_datetime.hour
+
+    def is_it_action_replacement_hour(self) -> bool:
+        return self.action_replacement_time.hour == self.cur_datetime.hour
--- src/simulation_env.py
+++ src/simulation_env.py
@@ -13,24 +13,11 @@ from src.prosumer import Prosumer
 from src.battery import Battery
 from src.energy_manipulation.energy_systems import EnergySystems
 from src.market import EnergyMarket
-from src.clock import Clock
+from src.clock import SimulationClock
+from src.constants import *
 
 
 ObservationType = Tuple[ObsType, float, bool, dict]
-
-SCHEDULING_TIME = time(hour=10, minute=30)
-ACTION_REPLACEMENT_TIME = time(hour=0)  # midnight
-START_TIME = datetime(
-    year=2022,
-    month=3,
-    day=8,
-    hour=SCHEDULING_TIME.hour,
-)
-
-BUY_AMOUNT_BOUND_HIGH = np.inf
-SELL_AMOUNT_BOUND_HIGH = np.inf
-BUY_PRICE_THRESHOLD_MAX = np.inf
-SELL_PRICE_THRESHOLD_MAX = np.inf
 
 
 class SimulationEnv(gym.Env):
@@ -39,44 +26,34 @@ class SimulationEnv(gym.Env):
             start_datetime: datetime = START_TIME,
             scheduling_time: time = SCHEDULING_TIME,
             action_replacement_time: time = ACTION_REPLACEMENT_TIME,
-            prices_df: pd.DataFrame,
+            # prices_df: pd.DataFrame,
             market_buy_price: float = 1.0,
             market_sell_price: float = 1.0,
     ):
-        self.action_space = self._action_space()
+        self.action_space = SIMULATION_ENV_ACTION_SPACE
         self.shape = (1, 1)
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=self.shape, dtype=np.float32)
 
         self.scheduling_time = scheduling_time
         self.action_replacement_time = action_replacement_time
-        self._clock = Clock(start_datetime=start_datetime, start_tick=0, tick_duration=timedelta(hours=1))
+        self._clock = SimulationClock(
+            start_datetime=start_datetime,
+            scheduling_time=scheduling_time,
+            action_replacement_time=action_replacement_time,
+            start_tick=0,
+            tick_duration=timedelta(hours=1)
+        )
 
         self.simulation = self._simulation()
         self.first_actions_set = False
 
         battery = Battery()
         energy_systems = EnergySystems()
-        self.energy_market = EnergyMarket()
+        self.energy_market = EnergyMarket(Currency(market_buy_price), Currency(market_sell_price))
         self.prosumer = Prosumer(battery, energy_systems, energy_market=self.energy_market)
 
         # start generator object
         self.simulation.send(None)
-
-    @staticmethod
-    def _action_space() -> spaces.Box:
-        # Action space of 24 energy amounts to trade and 24 price thresholds.
-        # Defines transactions for each hour for the next 24 hours
-        # starting from ACTION_REPLACEMENT_TIME
-        return spaces.Box(
-            low=np.array([0] * 96),
-            high=np.array(
-                [BUY_AMOUNT_BOUND_HIGH] * 24
-                + [SELL_AMOUNT_BOUND_HIGH] * 24
-                + [BUY_PRICE_THRESHOLD_MAX] * 24
-                + [SELL_PRICE_THRESHOLD_MAX] * 24
-            ),
-        )
-    # 24 buy amount 24 sell amount 24 price buy thresholds 24 price sell thresholds
 
     def reset(self, *, seed: Optional[int] = None, return_info: bool = False, options: Optional[dict] = None) -> Union[
         ObsType, Tuple[ObsType, dict]]:
@@ -90,11 +67,11 @@ class SimulationEnv(gym.Env):
 
     def _simulation(self) -> Generator[ObservationType, ActType, None]:
         while True:
-            if self._clock.is_it_time(self.scheduling_time):
+            if self._clock.is_it_scheduling_hour():
                 action = yield self._observation()
                 self.prosumer.schedule(action)
 
-            if self._clock.is_it_time(self.action_replacement_time):
+            if self._clock.is_it_action_replacement_hour():
                 are_actions_set = self.prosumer.set_new_actions()
                 if are_actions_set and not self.first_actions_set:
                     self.first_actions_set = True
