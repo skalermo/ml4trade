96dbc210b318007406b02f7aec60363d4ae7a2cf
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-04-09 19:55:15 +0200

    Fix typings

--- src/callback.py
+++ src/callback.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 from pandas import DataFrame
 
@@ -7,7 +7,7 @@ class Callback:
     def preprocess_data(self, df: DataFrame) -> None:
         raise NotImplementedError
 
-    def processed_columns(self) -> list[str]:
+    def processed_columns(self) -> List[str]:
         raise NotImplementedError
 
     def process(self, df: DataFrame, idx: int) -> Any:
