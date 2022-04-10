40f5b81c4e8bcb4cafaf6299d9c174e38c252d0e
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-04-10 23:25:14 +0200

    Introduce ClockView class

--- src/clock.py
+++ src/clock.py
@@ -1,11 +1,22 @@
 from datetime import datetime, time, timedelta
 
 
-from src.constants import SCHEDULING_TIME, ACTION_REPLACEMENT_TIME
+from src.constants import START_TIME, SCHEDULING_TIME, ACTION_REPLACEMENT_TIME
+
+
+class ClockView:
+    def __init__(self, clock: 'SimulationClock'):
+        self._clock = clock
+
+    def cur_datetime(self) -> datetime:
+        return self._clock.cur_datetime
+
+    def cur_tick(self) -> int:
+        return self._clock.cur_tick
 
 
 class SimulationClock:
-    def __init__(self, start_datetime: datetime,
+    def __init__(self, start_datetime: datetime = START_TIME,
                  scheduling_time: time = SCHEDULING_TIME, action_replacement_time: time = ACTION_REPLACEMENT_TIME,
                  start_tick: int = 0, tick_duration: timedelta = timedelta(hours=1)):
         self.cur_datetime = start_datetime
@@ -23,3 +34,6 @@ class SimulationClock:
 
     def is_it_action_replacement_hour(self) -> bool:
         return self.action_replacement_time.hour == self.cur_datetime.hour
+
+    def view(self) -> ClockView:
+        return ClockView(self)
