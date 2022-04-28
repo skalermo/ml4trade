9b0fdf2d74d4d51a76a8026ff08cbc3bd8fd1c5f
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-04-28 23:44:21 +0200

    Update tests

--- test/simulation_env/test_gym_interface.py
+++ test/simulation_env/test_gym_interface.py
@@ -8,7 +8,7 @@ from utils import setup_default_data_strategies
 
 class TestSimulationEnv(unittest.TestCase):
     def test_gym_interface_works(self):
-        env = SimulationEnv(setup_default_data_strategies(), start_tick=0)
+        env = SimulationEnv(setup_default_data_strategies())
 
         model = A2C('MlpPolicy', env, verbose=1)
         model.learn(total_timesteps=3)
@@ -19,7 +19,7 @@ class TestSimulationEnv(unittest.TestCase):
             obs, rewards, dones, info = env.step(action)
 
     def test_step_passes_action_to_prosumer(self):
-        env = SimulationEnv(setup_default_data_strategies(), start_tick=24)
+        env = SimulationEnv(setup_default_data_strategies())
         env.step(env.action_space.sample())
         self.assertIsNotNone(env.prosumer.scheduled_buy_amounts)
         self.assertIsNotNone(env.prosumer.scheduled_sell_amounts)
