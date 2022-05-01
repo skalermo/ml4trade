b730fa4b8d24ad3044ca1775a8bc08d8d5b1c9f6
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-05-01 22:04:18 +0200

    Rename current_charge -> init_charge

--- src/battery.py
+++ src/battery.py
@@ -2,12 +2,12 @@ from src.units import kWh
 
 
 class Battery:
-    def __init__(self, capacity: kWh = kWh(100), efficiency: float = 1.0, current_charge: kWh = kWh(0)):
+    def __init__(self, capacity: kWh = kWh(100), efficiency: float = 1.0, init_charge: kWh = kWh(0)):
         assert efficiency > 0, "Efficiency must be greater than 0"
 
         self.capacity = capacity
         self.efficiency = efficiency
-        self.current_charge = current_charge
+        self.current_charge = init_charge
 
     def charge(self, amount: kWh):
         charged_amount = min(amount * self.efficiency, self.capacity - self.current_charge)
