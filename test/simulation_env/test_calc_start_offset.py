db807794ead0bd7d5e306e6db7d63e7c98abc00f
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-06-01 08:31:55 +0200

    Fix calc_tick_offset() function

--- test/simulation_env/test_calc_start_offset.py
+++ test/simulation_env/test_calc_start_offset.py
@@ -6,7 +6,7 @@ from ml4trade.domain.constants import SCHEDULING_TIME
 from utils import setup_default_data_strategies
 
 
-class TestCalcStartIdx(unittest.TestCase):
+class TestCalcStartOffset(unittest.TestCase):
     def test_start_idx_points_to_scheduling_hour(self):
         dss = setup_default_data_strategies()
         start_idx = calc_tick_offset(list(dss.values()), SCHEDULING_TIME)
