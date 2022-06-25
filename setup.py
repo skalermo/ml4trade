c4c1d8baefb7b14073ff438ce23d4502f5c9da76
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-06-25 13:27:24 +0200

    Fix setup url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ml4trade',
     version='0.1.0',
-    url='https://github.com/skalermo/smartdiffer',
+    url='https://github.com/skalermo/ml4trade',
     author='skalermo',
     author_email='skalermo@gmail.com',
     description='Machine learning for trading',
