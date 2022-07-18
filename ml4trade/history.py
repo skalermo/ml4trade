8b23662853857cd8184a319748228d1bf28cc8a3
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-07-18 14:22:22 +0200

    Add save_path parameter

--- ml4trade/history.py
+++ ml4trade/history.py
@@ -131,8 +131,8 @@ class History:
         for key in tick_history_keys:
             self._history[key] = self._history[key][:-n or None]
 
-    def render(self, last_n_days: int = 2):
-        render_all(self._history, last_n_days)
+    def render(self, last_n_days: int = 2, save_path=None):
+        render_all(self._history, last_n_days, save_path)
 
     def save(self, path: str = 'env_history.json'):
         with open(path, 'w') as f:
