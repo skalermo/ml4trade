f87b2b019fc822ef22d9439721a1c5b509e6ce97
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-07-22 14:26:01 +0200

    Fill in missing rows in prices df

--- data/download_all.sh
+++ data/download_all.sh
@@ -4,6 +4,9 @@ mkdir -p ".data"
 energy_prices_link=https://drive.google.com/uc\?id\=1wU_zVNQoMtwc7XX1W3WZIbfBa5tuZ5oG\&export\=download
 wget -N -O ".data/prices_pl.csv" "$energy_prices_link"
 
+# Fill in missing rows
+python check_prices_df.py
+
 # download weather synoptic data from 2016-2022
 weather_link=https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/synop/
 for year in {2016..2022}
