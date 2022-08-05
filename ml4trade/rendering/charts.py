9ae50fc0844ae937f7f91672b731b1b62103c300
Agnieszka Ewa Dąbrowska <a.e.dabrowska@stud.elka.pw.edu.pl>
2022-08-06 00:52:28 +0200

    Remove unnecessary comment

--- ml4trade/rendering/charts.py
+++ ml4trade/rendering/charts.py
@@ -128,7 +128,6 @@ def _plot_scheduled_thresholds(history: pd.DataFrame, last_n_days: int, n_days_o
     ax2.plot(sells.index, sells, color='blue', label='sell threshold')
     ax2.plot(prices_history_last_n_days.index, prices_history_last_n_days, color='black', label='market price')
     ax2.set_ylim(bottom=min(prices_history_last_n_days) - 20)
-    # max(max(prices_history_last_n_days) + 20), max(buys) + 20, max(sells) + 20)
 
     if last_n_days <= 5:
         hours = mdates.HourLocator(byhour=list(range(0, 24, 4)), interval=1)
