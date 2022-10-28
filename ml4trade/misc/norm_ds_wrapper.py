b7c14b0071e82bc25354363f2794d79029104af6
Łukasz Lepak <lukasz.lepak.dokt@pw.edu.pl>
2022-10-28 12:03:56 +0000

    Modified consumption wrapper

--- ml4trade/misc/norm_ds_wrapper.py
+++ ml4trade/misc/norm_ds_wrapper.py
@@ -64,7 +64,7 @@ class ConsumptionWrapper(DataStrategyWrapper):
 
     def observation(self, idx: int) -> List[float]:
         obs = self.ds.observation(idx)
-        return list(map(lambda x: x * 1000, obs))
+        return list(map(lambda x: x * 1000 / self.ds.household_number, obs))
         # return obs
 
 
