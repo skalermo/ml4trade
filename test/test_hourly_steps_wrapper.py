0e0601b9ad5f47b09850b94607842369af372a77
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-12-02 22:42:14 +0100

    Update hourly_steps_wrapper tests

--- test/test_hourly_steps_wrapper.py
+++ test/test_hourly_steps_wrapper.py
@@ -1,6 +1,7 @@
 import unittest
 from datetime import timedelta
 
+from ml4trade.domain.clock import ClockView
 from ml4trade.domain.units import MWh
 from ml4trade.domain.constants import START_TIME
 from ml4trade.misc.hourly_steps_wrapper import HourlyStepsWrapper
@@ -10,18 +11,38 @@ from utils import setup_default_simulation_env
 class TestHourlyStepsWrapper(unittest.TestCase):
     def setUp(self) -> None:
         self.end_datetime = START_TIME + timedelta(days=14)
-        self.battery_init_charge = MWh(0.42)
+        self.battery_init_charge = MWh(0.1)
         self.env = HourlyStepsWrapper(setup_default_simulation_env(
             end_datetime=self.end_datetime,
             battery_init_charge=self.battery_init_charge
         ))
 
-    def test_run(self):
-        obs = self.env.reset()
-        for i in range(48):
+    def test_reset_preserves_env_hour(self):
+        env_clock_view: ClockView = self.env.new_clock_view()
+        self.env.reset()
+        self.env.step(self.env.action_space.sample())
+
+        obs, _ = self.env.reset()
+        wrapper_hour = obs[0]
+        self.assertEqual(wrapper_hour, env_clock_view.cur_datetime().hour)
+
+    def test_step_returns_correct_hour(self):
+        env_clock_view: ClockView = self.env.new_clock_view()
+
+        obs, _ = self.env.reset()
+        wrapper_hour = obs[0]
+        self.assertEqual(wrapper_hour, env_clock_view.cur_datetime().hour)
+
+        for i in range(3 * 24):
             action = self.env.action_space.sample()
-            print(i, action, obs)
-            obs = self.env.step(action)
+            obs, *_ = self.env.step(action)
+            wrapper_hour = obs[0]
+            self.assertEqual(wrapper_hour, env_clock_view.cur_datetime().hour)
+
+        self.env.reset()
+        obs, *_ = self.env.step(self.env.action_space.sample())
+        wrapper_hour = obs[0]
+        self.assertEqual(wrapper_hour, env_clock_view.cur_datetime().hour)
 
 
 if __name__ == '__main__':
