90bb5f0afac13342c0341555a39a150447344e0d
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-12-05 19:00:54 +0100

    Add discretized price from last day into observations

--- ml4trade/misc/hourly_steps_wrapper.py
+++ ml4trade/misc/hourly_steps_wrapper.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple, Optional
+from typing import Union, Tuple, Optional, List, NamedTuple
 
 import numpy as np
 from gymnasium.core import Wrapper, ObsType
@@ -11,6 +11,12 @@ from ml4trade.domain.clock import ClockView
 BIG_THRESHOLD = 99999
 
 
+class LastDayInfo(NamedTuple):
+    prices: List[float]
+    min_price: float
+    max_price: float
+
+
 class HourlyStepsWrapper(Wrapper):
     clock_view: ClockView
     _env: SimulationEnv
@@ -19,7 +25,7 @@ class HourlyStepsWrapper(Wrapper):
         super().__init__(env)
         self._env = env.unwrapped_env()
         self.action_space = Discrete(21)
-        self.observation_space = MultiDiscrete([24, 11, self.action_space.n])
+        self.observation_space = MultiDiscrete([24, 10, self.action_space.n, 10])
         self.clock_view = self.new_clock_view()
         self.current_hour = self.clock_view.cur_datetime().hour
         self.day_actions = np.zeros(24)
@@ -27,6 +33,8 @@ class HourlyStepsWrapper(Wrapper):
         self.last_action: Optional[int] = None
         self.last_wallet_balance: Optional[float] = 0
         self.use_dense_rewards = use_dense_rewards
+        # concerns time range 10:00-10:00
+        self.last_day_info: Optional[LastDayInfo] = None
 
     def _convert_to_original_action_space(self, day_actions: np.ndarray):
         res = np.zeros(96)
@@ -54,6 +62,7 @@ class HourlyStepsWrapper(Wrapper):
         self.saved_state = None
         self.last_action = None
         self.last_wallet_balance = None
+        self.last_day_info = None
         return self._observation(), {}
 
     def step(self, action: int) -> Tuple[ObsType, float, bool, bool, dict]:
@@ -79,6 +88,7 @@ class HourlyStepsWrapper(Wrapper):
                 self._env._clock.cur_datetime,
                 self._env._clock.cur_tick,
             )
+            self._update_last_day_info()
             self.day_actions = np.zeros(24)
         if self._env._first_actions_set:
             self._env._rand_produce_consume()
@@ -90,10 +100,35 @@ class HourlyStepsWrapper(Wrapper):
         self.last_wallet_balance = self._env._prosumer.wallet.balance.value
         return self._observation(), reward, terminated, truncated, {}
 
+    def _update_last_day_info(self):
+        history = self._env.history
+        end_idx = history._cur_tick_to_idx()
+        start_idx = end_idx - 24 or None
+        last_day_prices = list(map(lambda x: x['price'], history._history[start_idx:end_idx]))
+        if all(last_day_prices):
+            self.last_day_info = LastDayInfo(
+                prices=last_day_prices,
+                min_price=min(last_day_prices),
+                max_price=max(last_day_prices),
+            )
+
+    def _last_day_discretized_price(self, t: int) -> int:
+        if self.last_day_info is None:
+            return 5
+        price_at_t = self.last_day_info.prices[(t - 10 + 24) % 24]
+        price_after_conversion = np.interp(
+            price_at_t,
+            [self.last_day_info.min_price, self.last_day_info.max_price],
+            [0, 10],
+        )
+        discretized_price = min(int(price_after_conversion), 9)
+        return discretized_price
+
     def _observation(self) -> np.ndarray:
         predicted_rel_battery_charge = self._env._prosumer.battery.rel_current_charge
-        discretized_battery_charge = int(predicted_rel_battery_charge * 10)
-        obs = np.array([self.current_hour, discretized_battery_charge, self.last_action or 0])
+        discretized_battery_charge = min(int(predicted_rel_battery_charge * 10), 9)
+        discretized_price = self._last_day_discretized_price(self.current_hour)
+        obs = np.array([self.current_hour, discretized_battery_charge, self.last_action or 0, discretized_price])
         return obs
 
     def _reward(self) -> float:
