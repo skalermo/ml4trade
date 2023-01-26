74888b4afbc99026670fefedf54c8f7eef7803df
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-01-26 12:26:42 +0100

    Add test for render_profits_comparison() function

---
+++ test/rendering/test_render_profits_comparison.py
@@ -0,0 +1,25 @@
+import unittest
+
+import matplotlib.pyplot as plt
+
+from utils import rendering_data_paths
+
+from ml4trade.rendering.charts import render_profits_comparison
+from ml4trade.history import History
+
+
+class TestRenderProfitsComp(unittest.TestCase):
+    def setUp(self) -> None:
+        plt.show = lambda: ...
+
+    def test_plotting(self):
+        histories1 = [History.load(path) for path in rendering_data_paths[:2]]
+        histories2 = [History.load(path) for path in rendering_data_paths[2:]]
+        render_profits_comparison(
+            (histories1, {'color': 'red', 'label': 'bbb'}),
+            (histories2, {'color': 'cyan', 'label': 'aaa'}),
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
