1dd181bc882a1324613823db3157b57b10b2d046
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-05-19 15:23:52 +0200

    Include matplotlib into install requirements

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     author='skalermo',
     author_email='skalermo@gmail.com',
     description='Machine learning for trading',
-    packages=find_packages(),    
+    packages=find_packages(),
     install_requires=[
         'gymnasium',
         'pandas >= 1.3.5',
+        'matplotlib >= 3.5.3',
     ],
 )
