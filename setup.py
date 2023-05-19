a7bda20f5d282379329fafad6204bf0e9535088a
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-05-19 15:27:56 +0200

    Migrate to gymnasium (#76)
    * Remember wallet balance while intervals change
    
    * Add interval_start_ticks parameter to interval_wrapper
    
    * Revert commit "Remember wallet balance while intervals change"
    
    * Set version range to gym
    
    * History change
    
    * Fixed history
    
    * Changes to check for ml4trade
    
    * Changed actions range
    
    * Changed actions range
    
    * Changed action space
    
    * Set action space, reverted consumption observations
    
    * Modified consumption wrapper
    
    * Migrate to gymnasium
    
    * Update core functions and tests
    
    * Reduce amount of Nones in the first day of history
    
    * Change action bounds from [-10; 10] -> [-3; 3]
    
    * IJCAI'23 changes, added forecasts handling
    
    * Revert "Merge branch 'ijcai23' into gymnasium"
    
    This reverts commit c14b28d3029913b73da6d999e9856aa0d8dbd321, reversing
    changes made to 01c8c23697a3d68ec86c442cc81efe5337021cf1.
    
    * Update run.py
    
    * Update requirements.txt
    
    * Fix failing test
    
    * Include matplotlib into install requirements
    
    * Bump version to 0.2.0
    
    ---------
    
    Co-authored-by: adabrowska11 <agnieszka.dabrowska.11@gmail.com>
    Co-authored-by: Łukasz Lepak <lukasz.lepak.dokt@pw.edu.pl>
    Co-authored-by: Łukasz Lepak <llepak1996@gmail.com>

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml4trade',
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/skalermo/ml4trade',
     author='skalermo',
     author_email='skalermo@gmail.com',
     description='Machine learning for trading',
-    packages=find_packages(),    
+    packages=find_packages(),
     install_requires=[
-        'gym >=0.22.0, <0.26.0',
+        'gymnasium',
         'pandas >= 1.3.5',
+        'matplotlib >= 3.5.3',
     ],
 )
