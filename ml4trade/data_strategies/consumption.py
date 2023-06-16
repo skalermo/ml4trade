b4bc9f07de7377d2c8803db9bae0f05f0cc4fdbc
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-06-16 22:29:46 +0200

    Decouple power value calculation into separate function

--- ml4trade/data_strategies/consumption.py
+++ ml4trade/data_strategies/consumption.py
@@ -4,30 +4,38 @@ from ml4trade.data_strategies import DataStrategy, update_last_processed
 
 
 class HouseholdEnergyConsumptionDataStrategy(DataStrategy):
+    # avg energy consumption for 1 household
+    # for each hour of the day
+    energy_consumption_MWh = [
+        0.000189, 0.000184, 0.000181, 0.000181, 0.000182, 0.000185,
+        0.000194, 0.000222, 0.000238, 0.000246, 0.000246, 0.000246,
+        0.000248, 0.000249, 0.000246, 0.000244, 0.000244, 0.000244,
+        0.000243, 0.000246, 0.000246, 0.000242, 0.000225, 0.000208
+    ]
+
     def __init__(self, household_number: int = 1, window_size: int = 24):
         super().__init__(None, window_size, 'backward')
         self.household_number = household_number
 
-        # avg energy consumption for 1 household
-        # for each hour of the day
-        self.energy_consumption_MWh = [
-            0.000189, 0.000184, 0.000181, 0.000181, 0.000182, 0.000185,
-            0.000194, 0.000222, 0.000238, 0.000246, 0.000246, 0.000246,
-            0.000248, 0.000249, 0.000246, 0.000244, 0.000244, 0.000244,
-            0.000243, 0.000246, 0.000246, 0.000242, 0.000225, 0.000208
-        ]
-        # increase consumption to n households
-        self.energy_consumption_MWh = list(map(lambda x: x * self.household_number, self.energy_consumption_MWh))
-
+        # make data cyclic, so it can support accessing ranges beyond default 0:24
         extra_data = window_size // len(self.energy_consumption_MWh)
         self.energy_consumption_MWh += self.energy_consumption_MWh * extra_data
 
     @update_last_processed
     def process(self, idx: int) -> float:
-        consumed_energy = self.energy_consumption_MWh[idx % 24]
-        return consumed_energy * abs(1 + self.rng.normal(0, 0.03))
+        noise = self.rng.normal(0, 0.03)
+        return self.calculate_power_value(idx % 24, self.household_number, noise)
+
+    @staticmethod
+    def calculate_power_value(hour: int, household_number: int, noise: float):
+        consumed_energy = HouseholdEnergyConsumptionDataStrategy.energy_consumption_MWh[hour]
+        return consumed_energy * household_number * abs(1 + noise)
 
     def observation(self, idx: int) -> List[float]:
         start_idx = idx % 24 - self.scheduling_hour
         end_idx = start_idx + self.window_size
-        return self.energy_consumption_MWh[start_idx:end_idx]
+        obs = self.energy_consumption_MWh[start_idx:end_idx]
+
+        # increase consumption to n households
+        obs = list(map(lambda x: x * self.household_number, obs))
+        return obs
--- test/data_strategies/test_consumption.py
+++ test/data_strategies/test_consumption.py
@@ -1,5 +1,6 @@
 import unittest
 
+from ml4trade.data_strategies.consumption import HouseholdEnergyConsumptionDataStrategy
 from ml4trade.domain.units import MWh
 from utils import setup_default_consumption_system, setup_default_clock
 
@@ -32,6 +33,26 @@ class TestConsumption(unittest.TestCase):
         consumption_system = setup_default_consumption_system(window_size=26)
         data_length = len(consumption_system.ds.energy_consumption_MWh)
         self.assertEqual(data_length, 48)
+
+
+class TestDs(unittest.TestCase):
+    def test_hour_consumption(self):
+        household_number = 42
+        hour = 13
+        ds = setup_default_consumption_system(household_number=household_number).ds
+        v = ds.calculate_power_value(hour=hour, household_number=household_number, noise=0)
+        self.assertEqual(v, HouseholdEnergyConsumptionDataStrategy.energy_consumption_MWh[hour] * household_number)
+
+    def test_observation(self):
+        household_number = 42
+        window_size = 69
+        clock = setup_default_clock()
+        consumption_system = setup_default_consumption_system(clock, household_number=household_number, window_size=window_size)
+        obs = consumption_system.observation()
+        start = clock.cur_datetime.hour - 10
+        end = start + window_size
+        values = list(map(lambda x: x * household_number, HouseholdEnergyConsumptionDataStrategy.energy_consumption_MWh[start:end]))
+        self.assertEqual(obs, values)
 
 
 if __name__ == '__main__':
