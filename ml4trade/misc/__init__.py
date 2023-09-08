8cc8f820c8fbc339a6f3b3ec910b84f8b0e404fa
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-09-08 20:18:23 +0200

    Use avg prices over any interval in action wrapper

--- ml4trade/misc/__init__.py
+++ ml4trade/misc/__init__.py
@@ -1,3 +1,3 @@
 from ml4trade.misc.interval_wrapper import IntervalWrapper
-from ml4trade.misc.norm_action_wrapper import ActionWrapper, AvgMonthPriceRetriever
+from ml4trade.misc.norm_action_wrapper import ActionWrapper, AvgIntervalPriceRetriever
 from ml4trade.misc.norm_ds_wrapper import WeatherWrapper, MarketWrapper, ConsumptionWrapper
